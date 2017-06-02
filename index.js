f10da47a91663396eb03c77b8df909829e0708c6
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-06-02 23:26:44 +0200

    Add clipboard and bit.ly

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 'use strict';
-const alfy = require('alfy');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const Bitly = require('bitly');
+const Clipboardy = require('clipboardy');
+
+function IsURL(url) {
+	var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
+			+ "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+			+ "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184
+			+ "|" // 允许IP和DOMAIN（域名）
+			+ "([0-9a-z_!~*'()-]+\.)*" // 域名- www.
+			+ "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名
+			+ "[a-z]{2,6})" // first level domain- .com or .museum
+			+ "(:[0-9]{1,4})?" // 端口- :80
+			+ "((/?)|" // a slash isn't required if there is no file name
+			+ "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+	 var re=new RegExp(strRegex);
+	 return re.test(url);
+}
+
+var bitly = new Bitly('<YOUR ACCESS TOKEN>');
+
+var cb = Clipboardy.readSync();
+if(IsURL(cb)) {
+	bitly.shorten(cb)
+		.then(function(response) {
+			Clipboardy.writeSync(response.data.url);
+			console.log('Url was shortened');
+		}, function(error) {
+			console.log('Something went wrong');
+		});
+} else {
+	console.log('No url to shorten');
+}
