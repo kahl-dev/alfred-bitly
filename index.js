378b7bb232b86726fbde1badc9ed03993aa7ac8c
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-06-03 11:04:58 +0200

    Add access token as enviroment variable in alfred

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
+// Get access token or return
+const access_token = process.env.BITLY_ACCESS_TOKEN;
+if(!access_token) {
+	console.log('No access token defined');
+	return;
+}
+
+// Init bitly api
 const Bitly = require('bitly');
-const Clipboardy = require('clipboardy');
+var bitly = new Bitly(access_token);
 
 function IsURL(url) {
 	var strRegex = "^((https|http|ftp|rtsp|mms)?://)"
@@ -18,17 +26,19 @@ function IsURL(url) {
 	 return re.test(url);
 }
 
-var bitly = new Bitly('<YOUR ACCESS TOKEN>');
-
+// Get url from clipboard and check if is url or return
+const Clipboardy = require('clipboardy');
 var cb = Clipboardy.readSync();
-if(IsURL(cb)) {
-	bitly.shorten(cb)
-		.then(function(response) {
-			Clipboardy.writeSync(response.data.url);
-			console.log('Url was shortened');
-		}, function(error) {
-			console.log('Something went wrong');
-		});
-} else {
+if(!IsURL(cb)) {
 	console.log('No url to shorten');
+	return;
 }
+
+// Shorten the url and write it to clipboard
+bitly.shorten(cb)
+	.then(function(response) {
+		Clipboardy.writeSync(response.data.url);
+		console.log('Url was shortened');
+	}, function(error) {
+		console.log('Something went wrong');
+	});
