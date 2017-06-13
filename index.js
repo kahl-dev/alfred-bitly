7b27fe097f31aaece45657d5c13c4508df8378fb
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-06-14 00:22:54 +0200

    Remove xo & ava; Formate code with prettier

--- index.js
+++ index.js
@@ -4,58 +4,61 @@ const Bitly = require('bitly');
 const Clipboardy = require('clipboardy');
 
 class Alfred {
-	constructor() {
-		this.accessToken = process.env.BITLY_ACCESS_TOKEN;
-		this.clipboard = Clipboardy.readSync();
-	}
+  constructor() {
+    this.accessToken = process.env.BITLY_ACCESS_TOKEN;
+    this.clipboard = Clipboardy.readSync();
+  }
 
-	isUrl(url) {
-		const strRegex = '^((https|http|ftp|rtsp|mms)?://)' +
-			'?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' +
-			'(([0-9]{1,3}.){3}[0-9]{1,3}' +
-			'|' +
-			'([0-9a-z_!~*\'()-]+.)*' +
-			'([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' +
-			'[a-z]{2,6})' +
-			'(:[0-9]{1,4})?' +
-			'((/?)|' +
-			'(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$';
+  isUrl(url) {
+    const strRegex =
+      '^((https|http|ftp|rtsp|mms)?://)' +
+      "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" +
+      '(([0-9]{1,3}.){3}[0-9]{1,3}' +
+      '|' +
+      "([0-9a-z_!~*'()-]+.)*" +
+      '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' +
+      '[a-z]{2,6})' +
+      '(:[0-9]{1,4})?' +
+      '((/?)|' +
+      "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
 
-		const re = new RegExp(strRegex);
-		return re.test(url);
-	}
+    const re = new RegExp(strRegex);
+    return re.test(url);
+  }
 
-	isAccessToken() {
-		return Boolean(this.accessToken);
-	}
+  isAccessToken() {
+    return Boolean(this.accessToken);
+  }
 
-	run() {
-		// Check for access token
-		if (!this.isAccessToken()) {
-			console.log('No access token defined');
-			return;
-		}
+  run() {
+    // Check for access token
+    if (!this.isAccessToken()) {
+      console.log('No access token defined');
+      return;
+    }
 
-		// Init bitly api
-		const bitly = new Bitly(this.accessToken);
+    // Init bitly api
+    const bitly = new Bitly(this.accessToken);
 
-		// Get url from clipboard and check if is url or return
-		if (!this.isUrl(this.clipboard)) {
-			console.log('No url to shorten');
-			return;
-		}
+    // Get url from clipboard and check if is url or return
+    if (!this.isUrl(this.clipboard)) {
+      console.log('No url to shorten');
+      return;
+    }
 
-		// Shorten the url and write it to clipboard
-		bitly.shorten(this.clipboard)
-			.then(function (response) {
-				Clipboardy.writeSync(response.data.url);
-				console.log('Url was shortened');
-				return;
-			}, function () {
-				console.log('Something went wrong');
-				return;
-			});
-	}
+    // Shorten the url and write it to clipboard
+    bitly.shorten(this.clipboard).then(
+      function(response) {
+        Clipboardy.writeSync(response.data.url);
+        console.log('Url was shortened');
+        return;
+      },
+      function() {
+        console.log('Something went wrong');
+        return;
+      }
+    );
+  }
 }
 
 const wf = new Alfred();
