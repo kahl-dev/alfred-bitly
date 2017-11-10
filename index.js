676ca4a43c8bf6140de27559d7ec249d4716afab
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-11-10 09:01:29 +0100

    Replace url check from regex to valid-url npm package and Fixes #1

--- index.js
+++ index.js
@@ -1,66 +1,50 @@
-'use strict';
+'use strict'
 
-require('dotenv').config({path: __dirname + '/variables.env'});
-const Bitly = require('bitly');
-const Clipboardy = require('clipboardy');
+require('dotenv').config({ path: __dirname + '/variables.env' })
+const Bitly = require('bitly')
+const Clipboardy = require('clipboardy')
+const validUrl = require('valid-url')
 
 class Alfred {
   constructor() {
-    this.accessToken = process.env.BITLY_ACCESS_TOKEN;
-    this.clipboard = Clipboardy.readSync();
-  }
-
-  isUrl(url) {
-    const strRegex =
-      '^((https|http|ftp|rtsp|mms)?://)' +
-      "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" +
-      '(([0-9]{1,3}.){3}[0-9]{1,3}' +
-      '|' +
-      "([0-9a-z_!~*'()-]+.)*" +
-      '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' +
-      '[a-z]{2,6})' +
-      '(:[0-9]{1,4})?' +
-      '((/?)|' +
-      "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
-
-    const re = new RegExp(strRegex);
-    return re.test(url);
+    this.accessToken = process.env.BITLY_ACCESS_TOKEN
+    this.clipboard = Clipboardy.readSync()
   }
 
   isAccessToken() {
-    return Boolean(this.accessToken);
+    return Boolean(this.accessToken)
   }
 
   run() {
     // Check for access token
     if (!this.isAccessToken()) {
-      console.log('No access token defined');
-      return;
+      console.log('No access token defined')
+      return
     }
 
     // Init bitly api
-    const bitly = new Bitly(this.accessToken);
+    const bitly = new Bitly(this.accessToken)
 
     // Get url from clipboard and check if is url or return
-    if (!this.isUrl(this.clipboard)) {
-      console.log('No url to shorten');
-      return;
+    if (!validUrl.isUri(this.clipboard)) {
+      console.log('No url to shorten')
+      return
     }
 
     // Shorten the url and write it to clipboard
     bitly.shorten(this.clipboard).then(
       function(response) {
-        Clipboardy.writeSync(response.data.url);
-        console.log('Url was shortened');
-        return;
+        Clipboardy.writeSync(response.data.url)
+        console.log('Url was shortened')
+        return
       },
       function() {
-        console.log('Something went wrong');
-        return;
+        console.log('Something went wrong')
+        return
       }
-    );
+    )
   }
 }
 
-const wf = new Alfred();
-wf.run();
+const wf = new Alfred()
+wf.run()
